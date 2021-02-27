e07fad36603a54798c0d9c94d97faeaf7ced5f1b
Martin Dobrev <martin@dobrev.eu>
2021-02-27 19:42:40 +0000

    Add Jenkinspipe automation for arm64 platform

--- devops/buildPackage.groovy
+++ devops/buildPackage.groovy
@@ -3,13 +3,17 @@
 node("arm64") {
     stage('Setup node') {
         git url: 'https://github.com/mclueppers/alpine.git'
-        
+
         env.ALPINE_VERSION=params.ALPINE_VERSION
+        env.BUILDDIR='/home/ubuntu/alpine'
         env.PACKAGE_NAME=params.PACKAGE_NAME
         env.PLATFORM='linux/arm64'
+        sh """
+            mkdir -p .cache public
+            sudo chown ubuntu. .cache public -R
+        """
     }
-    
+
     stage("Build") {
         sh 'make package p=$PACKAGE_NAME'
     }
-}
